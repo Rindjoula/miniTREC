6445cdb207ef59c2fd731f7788b748503a65e199
rrabiaza <rindra.rabiazamaholy@etu.utc.fr>
2020-10-27 20:49:07 +0100

    add of stop words

--- src/org/apache/lucene/demo/SpanishAnalyzer2.java
+++ src/org/apache/lucene/demo/SpanishAnalyzer2.java
@@ -1,17 +1,23 @@
 package org.apache.lucene.demo;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.es.SpanishAnalyzer;
 import org.apache.lucene.analysis.snowball.SnowballFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 
 public class SpanishAnalyzer2 extends Analyzer {
 	protected TokenStreamComponents createComponents(String fieldName) {
 		final Tokenizer source = new StandardTokenizer();
-		TokenStream result = new LowerCaseFilter(source);
-		result = new SnowballFilter(result, "Spanish"); 
-		return new TokenStreamComponents(source, result);
+		CharArraySet stopSet = SpanishAnalyzer.getDefaultStopSet();
+		stopSet.add("desde");
+		TokenStream filter1 = new StopFilter(source, stopSet);
+		TokenStream filter2 = new LowerCaseFilter(filter1);
+		filter2 = new SnowballFilter(filter2, "Spanish");
+		return new TokenStreamComponents(source, filter2);
 	}
 }
