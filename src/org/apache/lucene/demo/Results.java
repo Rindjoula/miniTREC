aed3646c53ac20bb3d867f5ec5fd0f7714b30008
rrabiaza <rindra.rabiazamaholy@etu.utc.fr>
2020-10-29 13:20:01 +0100

    object with id of the query and id of the doc

---
+++ src/org/apache/lucene/demo/Results.java
@@ -0,0 +1,13 @@
+package org.apache.lucene.demo;
+
+public class Results {
+	String id_query;
+	String id_doc;
+	
+	public Results() {}
+	
+	public Results(String id_q, String id_d) {
+		id_query = id_q;
+		id_doc = id_d;
+	}
+}
