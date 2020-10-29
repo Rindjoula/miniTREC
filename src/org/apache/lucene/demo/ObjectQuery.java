3d07c1ef03931bd1b303b6b55c0ae4804d226344
rrabiaza <rindra.rabiazamaholy@etu.utc.fr>
2020-10-29 20:56:49 +0100

    add argument text_query + setter for query

--- src/org/apache/lucene/demo/ObjectQuery.java
+++ src/org/apache/lucene/demo/ObjectQuery.java
@@ -4,10 +4,15 @@ import org.apache.lucene.search.Query;
 
 public class ObjectQuery {
 	String id_query;
+	String text_query;
 	Query query;
 	
-	public ObjectQuery(String id, Query q) {
+	public ObjectQuery(String id, String text) {
 		id_query = id;
+		text_query = text;
+	}
+	
+	public void setQuery(Query q) {
 		query = q;
 	}
 }
