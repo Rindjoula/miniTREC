54e2e6bae6a69e8d91313d3fcfea87a43faec5ae
rrabiaza <rindra.rabiazamaholy@etu.utc.fr>
2020-10-29 20:57:08 +0100

    fix results of search files

--- src/org/apache/lucene/demo/SearchFiles.java
+++ src/org/apache/lucene/demo/SearchFiles.java
@@ -55,33 +55,15 @@ public class SearchFiles {
 	    String infoNeeds = null;
 	    String output = null;
 	    String queries = null;
-	    
-	    /** Creation of the file and directory results **/
-	    Path results_folder = Paths.get("results/");
-	    Files.createDirectory(results_folder);
-	    Date date_now = new Date(System.currentTimeMillis());
-	    Calendar calendar = Calendar.getInstance();
-	    calendar.setTime(date_now);
-	    String results_file_name = "results_" + Integer.toString(calendar.get(Calendar.YEAR))
-	    	+ Integer.toString(calendar.get(Calendar.MONTH))
-	    	+ Integer.toString(calendar.get(Calendar.DAY_OF_MONTH))
-	    	+ "_"
-	    	+ Integer.toString(calendar.get(Calendar.HOUR_OF_DAY))
-	    	+ Integer.toString(calendar.get(Calendar.MINUTE))
-	    	+ Integer.toString(calendar.get(Calendar.SECOND))
-	    	+ ".txt";
-	    
-	    File results_file = new File(results_folder.toString() + results_file_name);
-	    System.out.println("Results file name : " + results_file.toString());
-	    
-	    // TODO: replace by the number of queries of the xml_query_file
-	    Results[][] results = new Results[5][];
 	    	    
 	    int repeat = 0;
 	    boolean raw = false;
 	    String queryString = null;
 	    int hitsPerPage = 10;
 	    
+	    BufferedReader in = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+	    
+	    /** Parse all the parameters **/
 	    for(int i = 0;i < args.length;i++) {
 	      if ("-index".equals(args[i])) {
 	        index = args[i+1];
@@ -90,8 +72,8 @@ public class SearchFiles {
 	    	infoNeeds = args[i+1];
 	    	i++;
 	      } else if ("-output".equals(args[i])) {
-	    	output = args[i+1];
-	    	i++;
+	    	  output = args[i+1];
+	    	  i++;
 	      } else if ("-field".equals(args[i])) {
 	        field = args[i+1];
 	        i++;
@@ -120,10 +102,14 @@ public class SearchFiles {
 	    	System.err.println("Must provide infoNeeds file with -infoNeeds");
 		    System.exit(1);
 	    }
+	    
 	    if (output == null) {
 	    	System.err.println("Must provide output file with -output");
 		    System.exit(1);
 	    }
+	    
+	    /** Creation of the output file if does not exist **/
+	    File output_file = new File(output);
 	    
 	    System.out.println("PARSING XML INFONEEDS");
 	    
@@ -135,7 +121,7 @@ public class SearchFiles {
         org.w3c.dom.Document document = builder.parse(new File(infoNeeds));
         NodeList nodeList = document.getDocumentElement().getChildNodes();
         
-        ArrayList<String> informationNeeds = new ArrayList<String> ();
+        ArrayList<ObjectQuery> informationNeeds = new ArrayList<ObjectQuery> ();
         
         
         for (int i = 0; i < nodeList.getLength(); i++) {
@@ -155,54 +141,32 @@ public class SearchFiles {
 
                  System.out.println("ID: " + identifier + ", text: " + text);
                  
-                 informationNeeds.add(text);
+                 informationNeeds.add(new ObjectQuery(identifier, text));
                  
             }
-       }
+        }
+        
+        // TODO: replace by the number of queries of the xml_query_file
+	    ArrayList<ArrayList<Results>> results = new ArrayList<ArrayList<Results>>();
         
 	    
 	    IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(index)));
 	    IndexSearcher searcher = new IndexSearcher(reader);
 	    Analyzer analyzer = new SpanishAnalyzer2();
 
-	    BufferedReader in = null;
-	    if (queries != null) {
-	      in = new BufferedReader(new InputStreamReader(new FileInputStream(queries), "UTF-8"));
-	    } else {
-	      in = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-	    }
 	    QueryParser parser = new QueryParser(field, analyzer);
-	    for (int i=0; i<queries.length(); i++) {
-	      if (queries == null && queryString == null) {                        // prompt the user
-	        System.out.println("Enter query: ");
-	      }
-
-	      // todo: change for the XML queries
-	      String query_string = "title:Análisis de la evolucion económica de España desde la crisis de 2008 hasta 2019 en relacion con los diferentes partidos politicos que han gobernado el país durante este periodo o con cualquier otro aspecto de relevancia social."
-	    		  	+ " " + "subject:Análisis de la evolucion económica de España desde la crisis de 2008 hasta 2019 en relacion con los diferentes partidos politicos que han gobernado el país durante este periodo o con cualquier otro aspecto de relevancia social.";
-	      
-	      
-	      //String query_string = "title:evolucion";
-	      System.out.println(query_string);
-	            
-	      Query query = parser.parse(query_string);
-	      System.out.println(query.toString());
-	      System.out.println("Searching for: " + query.toString(field));
-	            
-	      /*if (repeat > 0) {                           // repeat & time as benchmark
-	        Date start = new Date();
-	        for (int i = 0; i < repeat; i++) {
-	          searcher.search(query, 100);
-	        }
-	        Date end = new Date();
-	        System.out.println("Time: "+(end.getTime()-start.getTime())+"ms");
-	      }*/
-	      
-	      // TODO: replace query_1 by id_query
-	      ObjectQuery obj_query = new ObjectQuery("query_1", query);
-	      
-	      // TODO: replace by a for loop on all the queries of xml_query_file
-	      results[0] = doPagingSearch(in, searcher, obj_query, hitsPerPage, raw, queries == null && queryString == null);
+	    for (ObjectQuery obj_q: informationNeeds) {
+	    	Query query = parser.parse(obj_q.text_query);
+	    	//System.out.println(query.toString());
+	    	System.out.println("Searching for: " + query.toString(field));
+	      	      
+	    	// TODO: set the argument query of obj_q --> readable by lucene, after the transformations...
+	    	// Query q = ...
+	    	// obj_q.setQuery(q);
+	    	
+	    	
+	    	// TODO: replace by a for loop on all the queries of xml_query_file
+	    	results.add(doPagingSearch(in, searcher, obj_q, hitsPerPage, raw, queries == null && queryString == null));
 	      
 	      //break;
 	      /*if (queryString != null) {
@@ -211,12 +175,12 @@ public class SearchFiles {
 	    }
 	    
 	    /** Write on the result file all the results **/
-	    FileWriter writer = new FileWriter(results_folder.toString() + results_file_name);
-	    for (int i=0; i < results.length; i++) {
-	    	for (int j=0; j<results[i].length; j++) {
-	    		String r = results[i][j].id_query + "\t" 
-	    				+ results[i][j].id_doc;
-	    		writer.write(r);
+	    FileWriter writer = new FileWriter(output);
+	    for (ArrayList<Results> res: results) {
+	    	for (Results r: res) {
+	    		String file_result = r.id_query + "\t" 
+	    				+ r.id_doc;
+	    		writer.write(file_result);
 	    		writer.write("\n");
 	    	}
 	    	
@@ -236,106 +200,35 @@ public class SearchFiles {
 	   * 
 	   * 
 	   */
-	  public static Results[] doPagingSearch(BufferedReader in, IndexSearcher searcher, ObjectQuery obj_query, 
+	  public static ArrayList<Results> doPagingSearch(BufferedReader in, IndexSearcher searcher, ObjectQuery obj_query, 
 	                                     int hitsPerPage, boolean raw, boolean interactive) throws IOException {
 
-		Query query = obj_query.query;
-	    // Collect enough docs to show 5 pages
-	    TopDocs results = searcher.search(query, 5 * hitsPerPage);
-	    ScoreDoc[] hits = results.scoreDocs;
+		  Query query = obj_query.query;
+		  // Collect enough docs to show 5 pages
+		  TopDocs results = searcher.search(query, 5 * hitsPerPage);
+		  ScoreDoc[] hits = results.scoreDocs;
 	    
-	    int numTotalHits = Math.toIntExact(results.totalHits.value);
-	    System.out.println(numTotalHits + " total matching documents");
+		  int numTotalHits = Math.toIntExact(results.totalHits.value);
+		  System.out.println(numTotalHits + " total matching documents");
+	    
+		  ArrayList<Results> results_query = new ArrayList<Results>();
+	    
+		  for (int i=0; i<numTotalHits; i++) {
 
-	    int start = 0;
-	    int end = Math.min(numTotalHits, hitsPerPage);
+			  Document doc = searcher.doc(hits[i].doc);
+		      String path = doc.get("path");
+		      if (path != null) {
+		    	  System.out.println((i+1) + ". " + path);
+		    	  Results r = new Results(obj_query.id_query, path);   
+		    	  results_query.add(r);
+		      }
 	        
-	    while (true) {
-	      if (end > hits.length) {
-	        System.out.println("Only results 1 - " + hits.length +" of " + numTotalHits + " total matching documents collected.");
-	        System.out.println("Collect more (y/n) ?");
-	        String line = in.readLine();
-	        if (line.length() == 0 || line.charAt(0) == 'n') {
-	          break;
-	        }
-
-	        hits = searcher.search(query, numTotalHits).scoreDocs;
-	      }
-	      
-	      end = Math.min(hits.length, start + hitsPerPage);
-	      
-	      for (int i = start; i < end; i++) {
-	        if (raw) {                              // output raw format
-	          System.out.println("doc="+hits[i].doc+" score="+hits[i].score);
-	          continue;
-	        }
-
-	        Document doc = searcher.doc(hits[i].doc);
-	        String path = doc.get("path");
-	        if (path != null) {
-	          System.out.println((i+1) + ". " + path);
-	          long modified = Long.parseLong(doc.get("modified"));
-	          if(raw == false & modified != 0) {
-	        	  System.out.println("modified: " + new Date(modified));
-	          }
-	          
-	          
-	          
-	        } else {
-	          System.out.println((i+1) + ". " + "No path for this document");
-	        }
-	        
-	        // explaining results
-	        System.out.println(searcher.explain(query, hits[i].doc));
+		      // explaining results
+		      //System.out.println(searcher.explain(query, hits[i].doc));
 	                  
 	      }
-
-	      if (!interactive || end == 0) {
-	        break;
-	      }
-
-	      if (numTotalHits >= end) {
-	        boolean quit = false;
-	        while (true) {
-	          System.out.print("Press ");
-	          if (start - hitsPerPage >= 0) {
-	            System.out.print("(p)revious page, ");  
-	          }
-	          if (start + hitsPerPage < numTotalHits) {
-	            System.out.print("(n)ext page, ");
-	          }
-	          System.out.println("(q)uit or enter number to jump to a page.");
-	          
-	          String line = in.readLine();
-	          if (line.length() == 0 || line.charAt(0)=='q') {
-	            quit = true;
-	            break;
-	          }
-	          if (line.charAt(0) == 'p') {
-	            start = Math.max(0, start - hitsPerPage);
-	            break;
-	          } else if (line.charAt(0) == 'n') {
-	            if (start + hitsPerPage < numTotalHits) {
-	              start+=hitsPerPage;
-	            }
-	            break;
-	          } else {
-	            int page = Integer.parseInt(line);
-	            if ((page - 1) * hitsPerPage < numTotalHits) {
-	              start = (page - 1) * hitsPerPage;
-	              break;
-	            } else {
-	              System.out.println("No such page");
-	            }
-	          }
-	        }
-	        if (quit) break;
-	        end = Math.min(numTotalHits, start + hitsPerPage);
-	      }
-	    }
-	    Results results_1 = new Results("query_1", "doc_1");
-	    Results[] results_query = new Results[numTotalHits];
-	    results_query[0] = results_1;
-	    return results_query;
+	  
+	  return results_query;
+	  
 	  }
 }
